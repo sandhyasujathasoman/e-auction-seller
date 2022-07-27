406599db1bf66c210487d7d0a3244965b5566f94
unknown <cogjava892@iiht.tech>
2022-07-27 13:20:47 +0530

    Updated jar

--- src/main/java/com/eauction/seller/EAuctionSellerServiceApplication.java
+++ src/main/java/com/eauction/seller/EAuctionSellerServiceApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan
+@EnableDiscoveryClient
+@EnableFeignClients
 @EnableConfigurationProperties({
 		BuyerServiceConfig.class
 })
